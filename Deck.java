d4e5d0aa5af5a821c75dd84cdb2020ac197dcc2b
ichakid <choirunnisa.fatima@gmail.com>
2014-04-22 11:27:24 +0700

    Update konstruktor, delete shuffleDeck
    Menambahkan semua card (PathCard + ActionCard kecuali StartCard dan GoalCard) ke dalam konstruktor Deck.
    Method shuffleDeck dihapus. Kenapa? karena tiap permainan, hanya melakukan satu kali shuffle/kocok kartu, yaitu saat kartu akan dibagikan ke para pemain. Oleh karena itu, shuffle dilakukan di konstruktor.

--- Deck.java
+++ Deck.java
@@ -1,4 +1,3 @@
-
 import java.util.Collections;
 import java.util.Stack;
 
@@ -18,6 +17,61 @@ public class Deck {
     
     public Deck(){
         PileOfCard = new Stack<Card>();
+        
+        for (int i=1; i<=4; i++)
+        {
+            PileOfCard.addElement(new Path_Card('0', '0', '1', '1', '1', 2));
+        }
+        for (int i=1; i<=5; i++)
+        {
+            PileOfCard.addElement(new Path_Card('1', '0', '1', '1', '1', 6));
+        }
+        for (int i=1; i<=5; i++)
+        {
+            PileOfCard.addElement(new Path_Card('1', '1', '1', '1', '1', 1));
+        }
+        for (int i=1; i<=4; i++)
+        {
+            PileOfCard.addElement(new Path_Card('1', '0', '1', '0', '1', 4));
+        }
+        for (int i=1; i<=5; i++)
+        {
+            PileOfCard.addElement(new Path_Card('0', '1', '1', '0', '1', 5));
+        }
+        PileOfCard.addElement(new Path_Card('0', '0', '1', '0', '1', 8));
+        PileOfCard.addElement(new Path_Card('0', '1', '1', '1', '0', 14));
+        PileOfCard.addElement(new Path_Card('1', '1', '1', '1', '0', 16));
+        PileOfCard.addElement(new Path_Card('1', '0', '1', '0', '0', 13));
+        PileOfCard.addElement(new Path_Card('0', '1', '1', '0', '0', 12));
+        PileOfCard.addElement(new Path_Card('0', '1', '0', '0', '1', 9));
+        for (int i=1; i<=5; i++)
+        {
+            PileOfCard.addElement(new Path_Card('1', '1', '0', '1', '1', 7));
+        }
+        for (int i=1; i<=3; i++)
+        {
+            PileOfCard.addElement(new Path_Card('1', '1', '0', '0', '1', 3));
+        }
+        PileOfCard.addElement(new Path_Card('0', '0', '1', '1', '0', 10));
+        PileOfCard.addElement(new Path_Card('1', '1', '0', '1', '0', 15));
+        PileOfCard.addElement(new Path_Card('1', '1', '0', '0', '0', 11));
+        
+        for (int i=1; i<=6; i++)
+        {
+            PileOfCard.addElement(new Action_Card(1));
+        }
+        for (int i=1; i<=9; i++)
+        {
+            PileOfCard.addElement(new Action_Card(2));
+        }
+        for (int i=1; i<=6; i++)
+        {
+            PileOfCard.addElement(new Action_Card(3));
+        }
+        
+        Collections.shuffle(PileOfCard);
+        
+        NumberOfAvailableCard = 61;
     }
     
     public Card popCard(){
@@ -31,7 +85,4 @@ public class Deck {
         NumberOfAvailableCard++;
     }
     
-    public void shuffleDeck(){
-        Collections.shuffle(PileOfCard);
-    }
 }
