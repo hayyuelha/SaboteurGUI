a16ef0e9971585a7e5a38b65e99fd74447527bff
Hayyu Hanifah <hayyuluthfi@students.itb.ac.id>
2014-04-23 21:10:44 +0700

    Kelas abstrak Card

---
+++ Card.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nim_13512080
+ */
+
+public abstract class Card {
+    public abstract void displayCard();
+    public abstract int getType(); //1=Path_Card, 2=Action_Card, 3=Character_Card
+    public abstract int getID();
+}
