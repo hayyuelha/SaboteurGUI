59f5781fa9ad57580d85d1c645c59690b92536fb
daniarherikurniawan <ddhhkk2@gmail.com>
2014-04-30 01:08:28 +0700

    update class Action_Card
    Sejauh ini masih blum ada perubahan yang berarti. aku cuma nambah
    definisi 'public' biar bisa diakses dari luar package

--- Action_Card.java
+++ Action_Card.java
@@ -1,15 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author nim_13512080
- */
-class Action_Card extends Card {
-    private int id; //1=ViewMap 2=break 3=repair
+public class Action_Card extends Card{
+	private int id; //1=ViewMap 2=break 3=repair
     public Action_Card(int i) {
         id = i;
     }
