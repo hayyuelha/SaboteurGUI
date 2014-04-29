060cb2c30d54be2287be9ebfb5b4113c3189a3eb
daniarherikurniawan <ddhhkk2@gmail.com>
2014-04-30 01:10:46 +0700

    update Deck.java [printDeck() kmu isi ya Cha..
    cuma aku rapiin spasinya dan ngitung jumlah kartunya ternyata 60 buah

--- Deck.java
+++ Deck.java
@@ -15,74 +15,55 @@ import java.util.Stack;
 public class Deck {
     private Stack<Card> PileOfCard;
     private int NumberOfAvailableCard;
+    private Card [][] aCard = new Card[5][9];
     
     public Deck(){
-        PileOfCard = new Stack<>();
+        PileOfCard = new Stack<Card>();
         NumberOfAvailableCard = 0;
     }
     public void fillDeck(){
-        PileOfCard = new Stack<>();
+        PileOfCard = new Stack<Card>();
         
-        for (int i=1; i<=4; i++)
-        {
-            PileOfCard.addElement(new Path_Card('0', '0', '1', '1', '1', 2));
-        }
-        for (int i=1; i<=5; i++)
-        {
-            PileOfCard.addElement(new Path_Card('1', '0', '1', '1', '1', 6));
-        }
-        for (int i=1; i<=5; i++)
-        {
-            PileOfCard.addElement(new Path_Card('1', '1', '1', '1', '1', 1));
-        }
-        for (int i=1; i<=4; i++)
-        {
-            PileOfCard.addElement(new Path_Card('1', '0', '1', '0', '1', 4));
-        }
-        for (int i=1; i<=5; i++)
-        {
+        for (int i=1; i<=4; i++){
+        	PileOfCard.addElement(new Path_Card('0', '0', '1', '1', '1', 2));
+        	PileOfCard.addElement(new Path_Card('1', '0', '1', '0', '1', 4));
+    	}
+        for (int i=1; i<=5; i++){
+        	PileOfCard.addElement(new Path_Card('1', '0', '1', '1', '1', 6));
+        	PileOfCard.addElement(new Path_Card('1', '1', '1', '1', '1', 1));
             PileOfCard.addElement(new Path_Card('0', '1', '1', '0', '1', 5));
+            PileOfCard.addElement(new Path_Card('1', '1', '0', '1', '1', 7));
         }
+        for (int i=1; i<=3; i++)
+        	PileOfCard.addElement(new Path_Card('1', '1', '0', '0', '1', 3));
+        for (int i=1; i<=6; i++){
+	        PileOfCard.addElement(new Action_Card(1));
+        	PileOfCard.addElement(new Action_Card(3));
+        }
+        for (int i=1; i<=9; i++)
+        	PileOfCard.addElement(new Action_Card(2));
+        
+        PileOfCard.addElement(new Path_Card('0', '0', '1', '1', '0', 10));
+        PileOfCard.addElement(new Path_Card('1', '1', '0', '1', '0', 15));
         PileOfCard.addElement(new Path_Card('0', '0', '1', '0', '1', 8));
         PileOfCard.addElement(new Path_Card('0', '1', '1', '1', '0', 14));
         PileOfCard.addElement(new Path_Card('1', '1', '1', '1', '0', 16));
         PileOfCard.addElement(new Path_Card('1', '0', '1', '0', '0', 13));
         PileOfCard.addElement(new Path_Card('0', '1', '1', '0', '0', 12));
         PileOfCard.addElement(new Path_Card('0', '1', '0', '0', '1', 9));
-        for (int i=1; i<=5; i++)
-        {
-            PileOfCard.addElement(new Path_Card('1', '1', '0', '1', '1', 7));
-        }
-        for (int i=1; i<=3; i++)
-        {
-            PileOfCard.addElement(new Path_Card('1', '1', '0', '0', '1', 3));
-        }
-        PileOfCard.addElement(new Path_Card('0', '0', '1', '1', '0', 10));
-        PileOfCard.addElement(new Path_Card('1', '1', '0', '1', '0', 15));
-        PileOfCard.addElement(new Path_Card('1', '1', '0', '0', '0', 11));
-        
-        for (int i=1; i<=6; i++)
-        {
-            PileOfCard.addElement(new Action_Card(1));
-        }
-        for (int i=1; i<=9; i++)
-        {
-            PileOfCard.addElement(new Action_Card(2));
-        }
-        for (int i=1; i<=6; i++)
-        {
-            PileOfCard.addElement(new Action_Card(3));
-        }
         
         Collections.shuffle(PileOfCard);
-        
-        NumberOfAvailableCard = 61;
+        NumberOfAvailableCard = 60;
     }
     
     public Card popCard(){
         Card result = PileOfCard.pop();
         NumberOfAvailableCard--;
         return result;
+    }
+    
+    public void  printDeck(){
+    	
     }
     
     public void pushCard(Card C){
