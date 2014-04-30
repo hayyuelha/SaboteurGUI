014f0371679dc4b606d93763c6f61d83b12db334
Daniar Heri Kurniawan <ddhhkk2@gmail.com>
2014-04-30 10:23:16 +0700

    mainsaboteur normal ini. tanpa ada gui
    ini cha

--- MainSaboteur.java
+++ MainSaboteur.java
@@ -1,31 +1,7 @@
-
 public class MainSaboteur {
     public static void main(String[] args) {
-        
-        System.setProperty(
-            "Quaqua.tabLayoutPolicy","wrap"
-         );
-
-         // set the Quaqua Look and Feel in the UIManager
-         try {
-              javax.swing.UIManager.setLookAndFeel("ch.randelshofer.quaqua.QuaquaLookAndFeel");
-         // set UI manager properties here that affect Quaqua
-         
-         } catch (Exception e) {
-             // take an appropriate action here
-         }
        
-        new eric.Console();
-        while(true){
-            System.out.println("Welcome to Java");
-            System.out.println("Enter A number");
-            
-            try { System.in.read();}
-            catch(Exception e){;}
-        
-            
-       //GamePlay Game = new GamePlay();
-       // Game.start();
-        }
+       GamePlay Game = new GamePlay();
+       Game.start();
     }
 }
