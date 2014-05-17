7ea377a5651309380d6fadea19d5d6b8c6a57029
khaidzir <dzir.shhh@gmail.com>
2014-05-17 13:55:48 -0700

    tes

---
+++ tes.java
@@ -0,0 +1,23 @@
+
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nim_13512080
+ */
+public class tes {
+
+    public static void main (String args[]){
+      Date dnow = new Date();
+      System.out.println(dnow.toString());
+      //gp.RandomRole(i);
+      
+    }
+
+}
